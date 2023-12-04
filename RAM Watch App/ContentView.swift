a46de741c8ff0d14a4dce04f4d6761648b5e3586
Amil <amilrampa2001@gmail.com>
2023-12-04 21:18:42 +0530

    implement ui

--- RAM Watch App/ContentView.swift
+++ RAM Watch App/ContentView.swift
@@ -10,10 +10,30 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            if ProcessInfo.processInfo.isLowPowerModeEnabled {
+                Image(systemName: "checkmark.seal")
+                    .foregroundStyle(.green, .yellow)
+                    .font(.system(size: 40))
+                Text("Low Power Mode")
+                    .foregroundColor(Color.yellow)
+                    .font(.system(size: 11))
+            } else {
+                Image(systemName: "checkmark.seal")
+                    .foregroundStyle(.green, .mint)
+                    .font(.system(size: 30))
+                    .padding()
+            }
+            VStack {
+                Text(String(ProcessInfo.processInfo.processorCount) + " CPUs")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Text(String(ProcessInfo.processInfo.physicalMemory / 1073741824) + " GB RAM")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Text(String(ProcessInfo.processInfo.activeProcessorCount) + " CPUs Active")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Text(String(ProcessInfo.processInfo.operatingSystemVersionString))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .font(.system(size: 11))
+            }.padding()
         }
         .padding()
     }
