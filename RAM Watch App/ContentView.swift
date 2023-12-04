5507d0ac0c0582cf3c1abe1692560dc1d1b84ddd
Amil <amilrampa2001@gmail.com>
2023-12-04 12:21:54 +0530

    Initial Commit

---
+++ RAM Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RAM Watch App
+//
+//  Created by Amil on 04/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RAM Watch App/RAMApp.swift
@@ -0,0 +1,17 @@
+//
+//  RAMApp.swift
+//  RAM Watch App
+//
+//  Created by Amil on 04/12/23.
+//
+
+import SwiftUI
+
+@main
+struct RAM_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
